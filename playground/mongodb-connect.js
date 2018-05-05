ea1252da518ea790f0fe20b182d407c857c062e6
Arunkumar Ramachandriya (Dell) <arun@dell>
2018-05-05 08:31:19 +0100

    Connect to db and insert a record

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     isCompleted: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo');
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'Arun',
+        age: 36,
+        phone: '223-665',
+        isResident: true
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user');
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    });
+
+    client.close();
+});

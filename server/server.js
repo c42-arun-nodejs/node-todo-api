b596b7b76cd70e3654a33e7c7a3b42a1d063a4cc
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-05-06 18:54:47 +0100

    mongooe model with validation and defaults

--- server/server.js
+++ server/server.js
@@ -5,29 +5,50 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-// can then update one of more fields
+// can then update zero or more fields (if no validation exists)
 // const newTodo = new Todo({
-//     text: 'Walk the dog',
 // });
 
 const newTodo = new Todo({
-    text: 'Walk the dog',
-    completed: true,
-    completedAt: 20180507
+    text: '  Eat lunch ',
 });
 
 newTodo.save()
     .then(res => {
         console.log('Todo saved:', res);
+    })
+    .catch(e => console.log(e));
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+const newUser = new User({
+    email: 'trtr'
+});
+
+newUser.save()
+    .then(res => {
+        console.log('User saved:', res);
     })
     .catch(e => console.log(e));
